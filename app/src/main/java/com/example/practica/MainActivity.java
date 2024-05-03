b137b27c431999a7028be691b303af0820b1ba88
qwerty <ilamudinov@gmail.com>
2024-05-03 11:39:00 +0300

    4 глава

--- app/src/main/java/com/example/practica/MainActivity.java
+++ app/src/main/java/com/example/practica/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.practica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -10,7 +11,33 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "QuestActivity";
+    private static final String KEY_INDEX = "index";
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() вызван");
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause() вызван");
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume() вызван");
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop() вызван");
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy() вызван");
+    }
     public Button mTrueButton;
     public Button mFalseButton;
     private ImageButton mNextButton;
@@ -22,6 +49,11 @@ public class MainActivity extends AppCompatActivity {
             new Question(R.string.question_service, false),
             new Question(R.string.question_res, true),
             new Question(R.string.question_manifest, true),
+            new Question(R.string.question_a, false),
+            new Question(R.string.question_b, true),
+            new Question(R.string.question_c, false),
+            new Question(R.string.question_d, true),
+            new Question(R.string.question_e, false),
     };
     private int mCurrentIndex = 0;
     private void updateQuestion() {
@@ -42,15 +74,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate(Bundle) вызван");
         setContentView(R.layout.activity_main);
+
         mQuestionTextView =
                 (TextView)findViewById(R.id.question_text_view);
+
+
         mTrueButton = (Button) findViewById(R.id.true_button);
+
+
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 checkAnswer(true);
-
             }
         });
         mFalseButton = (Button) findViewById(R.id.false_button);
@@ -58,7 +95,6 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 checkAnswer(false);
-
             }
         });
         mNextButton = (ImageButton)findViewById(R.id.next_button);
@@ -70,7 +106,6 @@ public class MainActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
-
         mBackButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -78,6 +113,16 @@ public class MainActivity extends AppCompatActivity {
                 updateQuestion();
             }
         });
+        if (savedInstanceState != null) {
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
+        }
         updateQuestion();
+
+    }
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
     }
 }
